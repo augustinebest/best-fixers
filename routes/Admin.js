51f1a466e8b20fe79532e0fb164bd9137f663494
Igwe Augustine <augustusbest00@gmail.com>
2018-11-10 14:56:02 -0900

    added a function that enables admin verifying a user

--- routes/Admin.js
+++ routes/Admin.js
@@ -7,6 +7,6 @@ router.post('/signup', adminController.signup);
 router.post('/login', adminController.login);
 router.get('/allRequest', adminController.getAllRequests);
 router.get('/allArtisans', adminController.getAllArtisansRequest);
-
+router.post('/:id/verify/', adminController.verifyAnArtisan);
 
 module.exports = router;
